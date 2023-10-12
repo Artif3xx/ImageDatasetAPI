044d2341a1648b4ed33a71c9601e445f7396131c
Castox <75119417+Artif3xx@users.noreply.github.com>
2023-10-12 21:37:34 +0200

    adding a image quality assessment script

---
+++ .scripts/IQA/NoReference.py
@@ -0,0 +1,16 @@
+class NoReference:
+    @staticmethod
+    def blindImageQualityIndex():
+        pass
+
+    @staticmethod
+    def naturalImageQualityEvaluator():
+        pass
+
+    @staticmethod
+    def visualInformationFidelity():
+        pass
+
+    @staticmethod
+    def visualSignalToNoiseRatio():
+        pass
---
+++ .scripts/IQA/ReducedReference.py
@@ -0,0 +1,16 @@
+class ReducedReference:
+    @staticmethod
+    def structuralSimilarityIndex():
+        pass
+
+    @staticmethod
+    def featureSimilarityIndex():
+        pass
+
+    @staticmethod
+    def visualInformationFidelity():
+        pass
+
+    @staticmethod
+    def visualSignalToNoiseRatio():
+        pass
