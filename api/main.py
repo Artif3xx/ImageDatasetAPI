9c382668b47acf56e5d56f89290c90f388997bbe
Castox <75119417+Artif3xx@users.noreply.github.com>
2023-10-21 01:06:27 +0200

    Revert "update main.py to merge it with front end"
    This reverts commit 43ff84dda4c5e814cee96951646c0a1c93afea48.
    
    # Conflicts:
    #	api/main.py

--- api/main.py
+++ api/main.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from starlette.templating import Jinja2Templates
 
 from api.src.endpoints import upload, image, info, item, search
-from api.src.database import models
-from api.src.database.database import engine
+
+from sqlalchemy.orm import Session
+
+from api.src.database import crud, models, schemas
+from api.src.database.database import get_db, engine
 
 models.Base.metadata.create_all(bind=engine)
 templates = Jinja2Templates(directory="api/templates")
@@ -13,7 +16,6 @@ templates = Jinja2Templates(directory="api/templates")
 # create a fastapi instance
 app = FastAPI()
 # mount the static folder to the api
-app.mount("/static", StaticFiles(directory="api/static/"), name="static")
 
 # include route inside the endpoints folder
 app.include_router(upload.router)
@@ -23,11 +25,12 @@ app.include_router(item.router)
 app.include_router(search.router)
 
 
-@app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
+@app.get("/")
+async def root():
     """
     Redirects to the docs page
 
     :return: a redirect to the docs page
     """
-    return templates.TemplateResponse("index.html", {'request': request})
+    return FileResponse("api/static/html/index.html")
+
