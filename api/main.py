956d60314a7900b77e996a446528b8287310dcc8
Castox <75119417+Artif3xx@users.noreply.github.com>
2023-10-24 18:05:37 +0200

    merge api-dev into frontend

--- api/main.py
+++ api/main.py
@@ -4,6 +4,7 @@ Main file of the fastapi. All the structured things are located here.
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from api.src.endpoints import upload, image, info, item, search
 from api.src.database import models
@@ -14,7 +15,9 @@ templates = Jinja2Templates(directory="api/templates")
 
 # create a fastapi instance
 app = FastAPI()
+
 # mount the static folder to the api
+app.mount("/static", StaticFiles(directory="api/static/"), name="static")
 
 # include route inside the endpoints folder
 app.include_router(upload.router)
